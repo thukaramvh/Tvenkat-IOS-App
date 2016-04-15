adebd9dae2b5fcfd4a360f766ad01150c25f9e44
Thukaram V H <venkat.thuka@gmail.com>
2016-04-15 01:07:31 -0700

    Tvenkat IOS App
    Tvenkat IOS App

---
+++ tvenkat/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  tvenkat
+//
+//  Created by Thukaram Venkatraman Hulugappa on 2/12/16.
+//  Copyright © 2016 www.tvenkat.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myWebView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /*let url = NSURL (string: "http://www.tvenkat.com");
+        let requestObj = NSURLRequest(URL: url!);
+        myWebView.loadRequest(requestObj);
+        */
+        
+        // let myWebView:UIWebView = UIWebView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height))
+        myWebView.scalesPageToFit = true
+        myWebView.keyboardDisplayRequiresUserAction = true
+        myWebView.loadRequest(NSURLRequest(URL: NSURL(string: "http://www.tvenkat.com")!))
+        self.view.addSubview(myWebView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        //myWebView.frame = UIScreen.mainScreen().bounds
+        //myWebView.center = self.view.center
+    }
+
+}
+
